98d5176073dbd0c2afce4468abe0f504b616d96a
Niklas Rast <n.rast@fraport.de>
2024-12-03 11:02:23 +0100

    feat(blog): added new-post.ps1 to create new post files and thumb.psd to create new thumbnail pictures in an matching layout for all posts

---
+++ _data/new-post.ps1
@@ -0,0 +1,20 @@
+$postId = (Read-Host -Prompt "Enter post number")
+$currentDate = Get-Date -Format "yyyy-MM-dd"
+$newFileName = "$currentDate-post-$postId.md"
+
+$content = @"
+---
+title: ''
+date: $currentDate 00:00:00
+featured_image: '/images/$postId/thumb.png'
+excerpt: 
+---
+
+# 
+
+![](/images/$postId/1.png)
+"@
+
+New-Item -Path "..\images\" -Name $postId -ItemType Directory
+New-Item -Path "..\_posts\$newFileName" -ItemType File
+Set-Content -Path "..\_posts\$newFileName" -Value $content
